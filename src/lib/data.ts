097cfa72bb38f66b2ba2491786db0b324b712833
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-05-26 01:21:09 +0900

    [이민희] 테스트 업데이트

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -14,21 +14,12 @@ export const workExperience = [
         "프로젝트 TH 팀에서 개발중입니다."
     ],
   },
-  {
-    company: "MakersGames",
-    location: "서울 강남구 학동로 211 (논현동) 삼현빌딩 3층 메이커스게임즈(주)",
-    position: "현장실습생",
-    period: "2019.6.21 - 2019.08.21",
-    achievements: [
-      "현장실습생 신분으로, 8주간 UI프리팹 리팩토링 및 제작에 참여하였습니다.",
-    ],
-  },
 ];
 
 export const education = [
   {
     institution: "한국산업기술대학교",
-    degree: "게임공학부 엔터테인먼트컴퓨팅학과 학사 졸업",
+    degree: "게임공학부 엔터테인먼트컴퓨팅학과",
     period: "2016.03 - 2021.02",
   },
 ];
@@ -46,14 +37,10 @@ export const projects = [
   {
     title: "B.B.B (Big Butt Bear",
     github: "https://github.com/baohaopoo/BBBproject",
-    description: [
-    "유니티로 만든 졸업작품 프로젝트입니다. Photon Cloud를 이용하여 서버 연동하였습니다.",
-    ],
   },
 ];
 
 export const awards = [
     {
-      
     },
 ];
