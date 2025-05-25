b8b85a85f8fb70410d0b9bc8014d7d87bae7cfbc
baohaopoo <53555682+baohaopoo@users.noreply.github.com>
2025-05-26 01:45:52 +0900

    [이민희] Hero섹션 수정

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -122,7 +122,7 @@ export default function HeroSection() {
           <div className="bg-gradient-to-r from-purple-500/10 to-pink-500/10 backdrop-blur-sm backdrop-filter p-4 rounded-lg border border-purple-500/20 dark:border-purple-500/10 shadow-sm">
             <p className="text-muted-foreground pl-4 py-2 mb-4 relative">
               <span className="absolute left-0 top-0 h-full w-1 bg-gradient-to-b from-purple-500 to-pink-500 rounded-full"></span>
-              🚀 안녕하세요 진격거의 후예를 이을 이민희입니다.
+              🚀Unreal Engine 5.4 기반 AAA 콘솔게임 클라이언트 프로그래머로 2년 반간 근무하며, 완성도 높은 게임 경험을 만드는 데 집중하고 있습니다. 많은 플레이어가 몰입할 수 있는 콘텐츠를 개발하는 것을 목표로 하고 있습니다.
             </p>
           </div>
         </MotionWrapper>
